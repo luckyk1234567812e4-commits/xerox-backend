f594b7288552dfad8754e72d4c4075297dffccbe
unknown <luckyk1234567812e4@gmail.com>
2025-09-18 20:37:17 +0530

    fix: use process.env.PORT for deployment

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000; // backend will run on port 5000
+const PORT = process.env.PORT || 5000;
+
 
 // middleware
 app.use(cors());
